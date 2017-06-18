077d62810c64de486f18a1f592a8736d87aabc06
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-06-19 01:43:49 +0530

    Home portfolio styling in process

--- development/scripts/index.js
+++ development/scripts/index.js
@@ -28,17 +28,66 @@ $(document).ready(function(){
 			}
 		});
 	});
-	$(window).scroll(function(){
+	// $(window).scroll(function(){
 
-		//Check the location of the declared element
-		$('.zoom').each(function(){
-			var bottom_of_object = $(this).position().top + 20; //+ $(this).outerHeight()
-			var bottom_of_window = $(window).scrollTop() + $(window).height();
+	// 	//Check the location of the declared element
+	// 	$('.zoom').each(function(){
+	// 		var bottom_of_object = $(this).position().top + 20; //+ $(this).outerHeight()
+	// 		var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-			// if the object is completely visible in the window, fade it in
-			if (bottom_of_window > bottom_of_object) {
-				$(this).animate(1000).addClass("w3-container w3-center w3-animate-zoom");
-			}
-		});
-	});
+	// 		// if the object is completely visible in the window, fade it in
+	// 		if (bottom_of_window > bottom_of_object) {
+	// 			$(this).animate(1000).addClass("w3-container w3-center w3-animate-zoom");
+	// 		}
+	// 	});
+	// });
+
+	var poptext = "#poptext";
+	var poptext2 = "#poptext2";
+	var bounce = "#bounce";
+	var bounce2 = "#bounce2";
+	var box = "#box";
+	var box2 = "#box2";
+	var clicked = true;
+	var clicked2 = true;
+
+		$(poptext).click(function () {
+
+				$(poptext).toggleClass("highlight");
+				
+				$(box).animate({
+					height: 'toggle',
+					opacity: 'toggle',
+					width: 200
+				}, 500);
+				if (clicked) {
+					$(bounce).removeClass("arrow bounce");
+					clicked = false;
+					return;
+				}
+				if (!clicked) {
+					$(bounce).addClass("arrow bounce");
+					clicked = true;
+				}
+			});
+	
+		$(poptext2).click(function () {
+
+				$(poptext2).toggleClass("highlight");
+				$(box2).animate({
+					height: 'toggle',
+					opacity: 'toggle',
+					width: 200
+				}, 500);
+				if (clicked2) {
+					$(bounce2).removeClass("arrow bounce");
+					clicked2 = false;
+					return;
+				}
+				if (!clicked2) {
+					$(bounce2).addClass("arrow bounce");
+					clicked2 = true;
+				}
+			});
+	
 });
